429e15d7376a86deac9799d3eac95a355226acbb
vinaya19 <vinaya.bomnale@gmail.com>
2026-01-26 17:12:39 -0600

    Removed comments

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,9 @@ from flask import Flask
 from config import Config
 from models import db
 from routes import bp
-# from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    # CORS(app)
     app.config.from_object(Config)
     db.init_app(app)
     app.register_blueprint(bp, url_prefix='/api')
